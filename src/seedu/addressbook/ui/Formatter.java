7dc9bb5bbf5d8a1b88f2e690fa5bd59fa9c550ad
eeeeasy <shixiyue97@gmail.com>
2016-09-02 08:03:06 +0800

    Extract out the responsibility of Formatting text for display from TextUi in to a separate class named Formatter

---
+++ src/seedu/addressbook/ui/Formatter.java
@@ -0,0 +1,47 @@
+package seedu.addressbook.ui;
+
+import java.util.List;
+
+public class Formatter {
+	
+    /** A decorative prefix added to the beginning of lines printed by AddressBook */
+    private static final String LINE_PREFIX = "|| ";
+
+    /** A platform independent line separator. */
+    private static final String LS = System.lineSeparator();
+
+    /** Format of indexed list item */
+    private static final String MESSAGE_INDEXED_LIST_ITEM = "\t%1$d. %2$s";
+
+    /** Offset required to convert between 1-indexing and 0-indexing.  */
+    public static final int DISPLAYED_INDEX_OFFSET = 1;
+
+    /** Shows message(s) to the user */
+    public static String formatMessage(String message) {
+    	return getLinePrefix() + message.replace("\n", LS + getLinePrefix());
+    }
+
+	public static String getIndexedListForViewing(List<String> listItems) {
+        final StringBuilder formatted = new StringBuilder();
+        int displayIndex = 0 + DISPLAYED_INDEX_OFFSET;
+        for (String listItem : listItems) {
+            formatted.append(getIndexedListItem(displayIndex, listItem)).append("\n");
+            displayIndex++;
+        }
+        return formatted.toString();
+    }
+
+    /**
+     * Formats a string as a viewable indexed list item.
+     *
+     * @param visibleIndex visible index for this listing
+     */
+    private static String getIndexedListItem(int visibleIndex, String listItem) {
+        return String.format(MESSAGE_INDEXED_LIST_ITEM, visibleIndex, listItem);
+    }
+
+	public static String getLinePrefix() {
+		return LINE_PREFIX;
+	}
+
+}
